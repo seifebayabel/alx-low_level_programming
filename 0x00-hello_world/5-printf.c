c3dde006a2e1e866f33ea352744dece64ab532a5
seifebayabel <seife.bayabel@gmail.com>
2022-12-08 14:14:58 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,7 +6,7 @@
  */
 int main(void)
 {
-        puts("with proper grammar,but the outcome is a piece of art,\n");
+        puts("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
