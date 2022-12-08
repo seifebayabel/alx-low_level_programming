75e62cdfc24748d4deaca6561dc67f0eb395b867
seifebayabel <seife.bayabel@gmail.com>
2022-12-08 10:56:10 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
-int main(void)
-{
-   puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
