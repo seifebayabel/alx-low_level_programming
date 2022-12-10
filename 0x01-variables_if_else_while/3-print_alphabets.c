010a3d7f4cec0819c7c756104bd891985b2fe829
seifebayabel <seife.bayabel@gmail.com>
2022-12-10 11:07:14 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Description: print the alphabet in lower case
+ * and the uppercase, follow by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		c = 'A';
+		while (c <= 'Z')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
