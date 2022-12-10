feda8188f5ea13f609354534b06dd49ab60d5d7c
seifebayabel <seife.bayabel@gmail.com>
2022-12-10 13:48:38 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include  <time.h>
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: get a random it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
