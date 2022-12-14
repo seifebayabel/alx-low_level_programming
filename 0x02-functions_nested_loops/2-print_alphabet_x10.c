914f50af7857637fa911b5a642b2c0a508d30988
seifebayabel <seife.bayabel@gmail.com>
2022-12-13 23:23:38 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alhpabet_x10 - print alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char i;
+
+	while (count++ <= 9)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
