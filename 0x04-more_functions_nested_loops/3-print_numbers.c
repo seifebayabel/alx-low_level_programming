5f7442db62c667af3e9596e427dfc08db2062c11
seifebayabel <seife.bayabel@gmail.com>
2022-12-16 03:21:36 -0800

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - the function to print numbers from 0 - 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
