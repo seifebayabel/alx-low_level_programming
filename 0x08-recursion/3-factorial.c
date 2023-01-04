8527007ac49a1fdead634f6f3332320b877dc4d1
seifebayabel <seife.bayabel@gmail.com>
2023-01-04 10:38:18 -0800

    3-factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,10 +7,10 @@
  */
 int factorial(int n)
 {
-	int result = n;
-
-	if (n < 0)
-		return (-1);
-	else if (n >= 0 && n <= 1)
+	if (n == 0)
 		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
 }
