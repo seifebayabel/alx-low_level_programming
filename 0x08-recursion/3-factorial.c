d8a80c9deead237c95117399eec6404df224632d
seifebayabel <seife.bayabel@gmail.com>
2023-01-04 10:29:12 -0800

    2-strlen_recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial a given number
+ * @n: The number to the factorial
+ * Return: its value
+ */
+int factorial(int n)
+{
+	int result = n;
+
+	if (n < 0)
+		return (-1);
+	else if (n >= 0 && n <= 1)
+		return (1);
+}
