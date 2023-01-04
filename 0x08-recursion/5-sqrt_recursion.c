75b2a348eeafd5cb8ecf77825b32ee362b3b6a27
seifebayabel <seife.bayabel@gmail.com>
2023-01-04 12:15:59 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculate the square of root a number
+ * @n: integer
+ * Return:sgrt recursion
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0)
+	return (-1);
+
+	else
+	{
+	return (_evaluate(i, n));
+	}
+}
